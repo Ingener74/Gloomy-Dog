041f648f142b3a586a529160a4ce37e557b3b93e
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-12-16 20:22:35 +0300

    добавил файлы

--- CopyJavaFiles.cmake
+++ CopyJavaFiles.cmake
@@ -5,7 +5,15 @@ macro(CopyJavaFiles target destination)
         CopyJavaFiles${} ALL
         COMMAND rm -rf ${destination}
         COMMAND mkdir -p ${destination}
-        COMMAND rsync -a --exclude="*.jar" --exclude="*.h" --exclude="*.cxx" --exclude="*/" ${CMAKE_CURRENT_BINARY_DIR}/* ${destination}
+        COMMAND rsync -a \
+            --exclude="*.jar" \
+            --exclude="*.h" \
+            --exclude="*.cxx" \
+            --exclude="Makefile" \
+            --exclude="*.so" \
+            --exclude="*.cmake" \
+            --exclude="*/" \
+            ${CMAKE_CURRENT_BINARY_DIR}/* ${destination}
         DEPENDS ${target}
     )
 endmacro()
